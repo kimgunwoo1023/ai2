0a888a169c49bbf793bccbb0a52147cc631f8c4a
kimgunwoo1023 <20242801@sdhs.gwe.hs.kr>
2024-11-20 10:10:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1UcgSPkDW9cLiSr4r2Fna8jCzArWljcQn/view?usp=sharing'
+file_id = '1UcgSPkDW9cLiSr4r2Fna8jCzArWljcQn'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
